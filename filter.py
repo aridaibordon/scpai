9749cdb5a97eedd388b249e7afe7ade81f54c470
Aridai Bordón <aridai.bordon@ulpgc.es>
2025-02-10 10:56:49 +0000

    fix: correct convolution with instrument

--- filter.py
+++ filter.py
@@ -2,8 +2,8 @@ import numpy as np
 import scipy.signal as sgn
 
 
-def normal_filter(signal, delta_E: float):
-    x = np.linspace(-4 * delta_E, 4 * delta_E, 12 * delta_E)
+def apply_instrument_resolution(egrid, signal, delta_E: float):
+    x = np.arange(-4 * delta_E, 4 * delta_E, egrid[1] - egrid[0])
     gaussian = np.exp(-0.5 * (x / delta_E) ** 2)
 
     return sgn.fftconvolve(signal, gaussian, mode="same")
