459fd785656938f40bf1e0322210a466112d5d24
Aridai Bordón <aridai.bordon@ulpgc.es>
2025-02-19 11:54:36 +0000

    update: add multizone model

--- model.py
+++ model.py
@@ -4,6 +4,10 @@ import torch.nn as nn
 class SCPAI_H(nn.Module):
     def __init__(self, egrid: list) -> None:
         super().__init__()
+
+        self.name = "SCPAI_H"
+        self.description = "SCPAI for homogeneous plasmas"
+
         self.model = nn.Sequential(
             nn.Linear(len(egrid), 512),
             nn.ReLU(),
@@ -12,6 +16,27 @@ class SCPAI_H(nn.Module):
             nn.Linear(512, 512),
             nn.ReLU(),
             nn.Linear(512, 3),
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class SCPAI_MZ(nn.Module):
+    def __init__(self, egrid: list, nzones: int) -> None:
+        super().__init__()
+
+        self.name = "SCPAI_MZ"
+        self.description = f"SCPAI for multizone analysis ({nzones} zones)"
+
+        self.model = nn.Sequential(
+            nn.Linear(len(egrid), 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 3 * nzones),
         )
 
     def forward(self, x):
